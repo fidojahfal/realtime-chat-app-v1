4babb3f16a46646eb23aae9a97a836aa5eef954a
fidojahfal <fidojahfalprayoga@gmail.com>
2026-05-10 08:42:27 +0700

    feat: initialization zod validation for validating any input

--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { ZodService } from './zod/zod.service';
+import { ValidationService } from './validation/validation.service';
 
 @Module({
-  providers: [PrismaService],
+  providers: [PrismaService, ZodService, ValidationService],
   controllers: [],
 })
 export class CommonModule {}
