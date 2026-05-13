742af88d96a5d82bf7cff08ecd5d02d5854d32f5
fidojahfal <fidojahfalprayoga@gmail.com>
2026-05-13 07:03:43 +0700

    feat: create get message on chat service

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/prisma/prisma.service';
+import { Message } from '../model/chat.model';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getMessage(user_id: number, conversation_id: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+
+    if (!user) {
+      throw Error('User not found');
+    }
+
+    const conversation = await this.prismaService.conversation.findUnique({
+      where: {
+        id: conversation_id,
+      },
+    });
+
+    if (!conversation) {
+      throw Error('Conversation not found');
+    }
+
+    const messages = await this.prismaService.message.findMany({
+      where: {
+        AND: [
+          {
+            user_id: user_id,
+          },
+          {
+            conversation_id: conversation_id,
+          },
+        ],
+      },
+    });
+
+    return messages;
+  }
+}
