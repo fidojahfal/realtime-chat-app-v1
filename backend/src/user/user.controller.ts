0bdc19658e79d29b15a656d277990a2ec6ff6355
fidojahfal <fidojahfalprayoga@gmail.com>
2026-05-15 05:43:23 +0700

    feat: create function user service to get user by id

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { User } from '../model/user.model';
+import { WebResponse } from '../model/web.model';
+import { UserService } from './user.service';
 
-@Controller()
-export class UserController {}
+@Controller('/user')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('/getUser/:id')
+  @HttpCode(200)
+  async getUser(@Param('id') id: number): Promise<WebResponse<User>> {
+    const user = await this.userService.getUser(id);
+
+    return {
+      data: user,
+    };
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/prisma/prisma.service';
+import { User } from '../model/user.model';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
+
+  async getUser(id: number): Promise<User> {
+    const user: User = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw Error('User not found');
+    }
+
+    return {
+      id: user.id,
+      username: user.username,
+      name: user.name,
+      email: user.email,
+    };
+  }
 }
