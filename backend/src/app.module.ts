40ab9bed85416b4110722df6e121f413367b37fc
fidojahfal <fidojahfalprayoga@gmail.com>
2026-05-17 17:01:37 +0700

    fix: error linter on app module 'cannot find the exact service'

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonService } from './common/common.service';
 import { CommonModule } from './common/common.module';
 import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
