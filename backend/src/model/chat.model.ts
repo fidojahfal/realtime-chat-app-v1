34f50dc76530e2770a4c655a82f8932be1422fc2
fidojahfal <fidojahfalprayoga@gmail.com>
2026-05-17 17:01:57 +0700

    feat: separate model for chat to become request and response model

--- backend/src/model/chat.model.ts
+++ backend/src/model/chat.model.ts
@@ -1,5 +1,11 @@
-export class Message {
+export class MessageResponse {
   id: number;
+  conversation_id: number;
+  user_id: number;
+  message: string;
+}
+
+export class CreateMessageRequest {
   conversation_id: number;
   user_id: number;
   message: string;
