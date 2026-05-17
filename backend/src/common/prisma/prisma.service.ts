aae04b3cd7a435392bb3274e23ab8b17fab6a53c
fidojahfal <fidojahfalprayoga@gmail.com>
2026-05-17 21:58:47 +0700

    fix: bug prisma won't run because need to define adapter manually since prisma 7

--- backend/src/common/prisma/prisma.service.ts
+++ backend/src/common/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient, Prisma } from '@prisma/client';
 
 @Injectable()
@@ -6,6 +7,14 @@ export class PrismaService
   extends PrismaClient<Prisma.PrismaClientOptions, string>
   implements OnModuleInit
 {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
